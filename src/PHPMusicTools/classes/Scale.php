3e3b5c0ec2c1d8c6055b89ddf981d1707c7052d7
Ring, Ian <iring@netsuite.com>
2016-09-25 11:19:58 -0400

    readme for tests

--- src/PHPMusicTools/classes/Scale.php
+++ src/PHPMusicTools/classes/Scale.php
@@ -167,19 +167,116 @@ class Scale extends PMTObject {
 
 	}
 
-	public function modes() {
-
+	public static function findImperfections($scale) {
+		$imperfections = array();
+		for ($i = 0; $i<12; $i++) {
+			$fifthAbove = ($i + 7) % 12;
+			if ($scale & (1 << $i) && !($scale & (1 << $fifthAbove))) {
+				$imperfections[] = $i;
+			}
+		}
+		return $imperfections;
 	}
 
-	public function symmetries() {
-		$rotateme = $this->scale;
-		$symmetries = array();
+	/**
+	 * returns the spectrum of a scale, ie how many of every type of interval exists between all the tones.
+	 * @param  [type] $scale [description]
+	 * @return [type]        [description]
+	 */
+	public static function findSpectrum($scale) {
+		$spectrum = array();
+		$rotateme = $scale;
+		for ($i=0; $i<6; $i++) {
+			$rotateme = rotateBitmask($rotateme, $direction = 1, $amount = 1);
+			$spectrum[$i] = countOnBits($scale & $rotateme);
+		}
+		// special rule: if there is a tritone in the sonority, it will show up twice, so we divide by 2
+		$spectrum[5] = $spectrum[5] / 2;
+		return $spectrum;
+	}
+
+	/**
+	 * takes a scale spectrum, and renders the "pmn" summmary string, ala Howard Hansen
+	 * @param  [type] $spectrum [description]
+	 * @return [type]           [description]
+	 */
+	function renderPmn($spectrum) {
+		$string = '';
+		// remember these are 0-based, so they're like the number of semitones minus 1
+		$classes = array('p' => 4, 'm' => 3, 'n' => 2, 's' => 1, 'd' => 0, 't' => 5);
+		foreach ($classes as $class => $interval) {
+			if ($spectrum[$interval] > 0) {
+				$string .= $class;
+			}
+			if ($spectrum[$interval] > 1) {
+				$string .= '<sup>'.$spectrum[$interval].'</sup>';
+			}
+		}
+		return '<em>' . $string . '</em>';
+	}
+
+	/**
+	 * a special rule that some people think defines what a scale is.
+	 * returns true if the first bit is not a zero.
+	 * Useful for filtering a set of numbers to weed out non-scales
+	 * @param  [type] $scale [description]
+	 * @return [type]        [description]
+	 */
+	function hasRootTone($scale) {
+		// returns true if the first bit is not a zero
+		return (1 & $scale) != 0;
+	}
+
+	/**
+	 * a special rule that some people think defines what a scale is, ie not having leaps of more than a major third.
+	 * Useful for filtering a set of numbers to weed out non-scales
+	 * @param  [type] $scale [description]
+	 * @return [type]        [description]
+	 */
+	public static function doesNotHaveFourConsecutiveOffBits($scale) {
+		$c = 0;
+		for ($i=0; $i<12; $i++) {
+			if (!($scale & (1 << ($i)))) {
+				$c++;
+				if ($c >= 4) {
+					return false;
+				}
+			} else {
+				$c = 0;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * returns an array of all the modes of a scale.
+	 * @param  [type] $scale [description]
+	 * @return [type]        [description]
+	 */
+	function modes($scale) {
+		$rotateme = $scale;
+		$modes = array();
 		for ($i = 0; $i < 12; $i++) {
-			$rotateme = $this->rotate_bitmask($rotateme);
+			$rotateme = rotateBitmask($rotateme);
 			if (($rotateme & 1) == 0) {
 				continue;
 			}
-			if ($rotateme == $this->scale) {
+			$modes[] = $rotateme;
+		}
+		return $modes;
+	}
+
+	/**
+	 * finds the notes of a scale that are symmetry axes, ie the roots of modes that are identical the original
+	 * @param  [type] $scale [description]
+	 * @return [type]        [description]
+	 */
+	function symmetries($scale) {
+		$rotateme = $scale;
+		$symmetries = array();
+		for ($i = 0; $i < 12; $i++) {
+			$rotateme = rotateBitmask($rotateme);
+			if ($rotateme == $scale) {
 				if ($i != 11) {
 					$symmetries[] = $i+1;
 				}
@@ -188,6 +285,10 @@ class Scale extends PMTObject {
 		return $symmetries;
 	}
 
+	/**
+	 * returns true if a scale is palindromic
+	 * @return boolean [description]
+	 */
 	public function isPalindromic() {
 		for ($i=1; $i<=5; $i++) {
 			if ( (bool)($this->scale & (1 << $i)) !== (bool)($this->scale & (1 << (12 - $i))) ) {
@@ -197,10 +298,14 @@ class Scale extends PMTObject {
 		return true;
 	}
 
-	public function countTones() {
+	/**
+	 * counts the number of tones in a scale
+	 * @return [type] [description]
+	 */
+	public static function countTones($scale) {
 		$tones = 0;
 		for ($i = 0; $i < 12; $i++) {
-			if (($this->scale & (1 << $i)) == 0) {
+			if (($scale & (1 << $i)) == 0) {
 				$tones++;
 			}
 		}
@@ -221,8 +326,8 @@ class Scale extends PMTObject {
 		return null;
 	}
 
-	private function rotate_bitmask($bits, $direction = 1, $howfar = 1) {
-		for ($i=0; $i<$howfar; $i++) {
+	function rotateBitmask($bits, $direction = 1, $amount = 1) {
+		for ($i = 0; $i < $amount; $i++) {
 			if ($direction == 1) {
 				$firstbit = $bits & 1;
 				$bits = $bits >> 1;
@@ -232,10 +337,82 @@ class Scale extends PMTObject {
 				$bits = $bits << 1;
 				$bits = $bits & ~(1 << 12);
 				$bits = $bits | ($firstbit >> 11);
-
 			}
 		}
 		return $bits;
+	}
+
+	function drawSVGBracelet($scale, $size = 200, $text = null, $showImperfections = false) {
+		if ($showImperfections) {
+			$imperfections = findImperfections($scale);
+			$symmetries = symmetries($scale);
+		}
+
+		$s = '';
+		if ($size > 100) {
+			$stroke = 3;
+		} elseif ($size > 50) {
+			$stroke = 2;
+		} else {
+			$stroke = 1;
+		}
+		$smallrad = floor(($size / 12));
+		$centerx = $size / 2;
+		$centery = $size / 2;
+		$radius = floor( ($size - ($smallrad*2) - ($stroke*4)) / 2 );
+		$s .= '<svg xmlns="http://www.w3.org/2000/svg" height="'. ($size + 3).'" width="'.($size + 3) .'">';
+		$s .= '<circle r="'.$radius.'" cx="'.$centerx.'" cy="'.$centery.'" stroke-width="'.$stroke.'" fill="white" stroke="black"/>';
+		$symmetryshape = array();
+		for ($i=0; $i<12; $i++) {
+
+			$deg = $i * 30 - 90;
+			$x1 = floor($centerx + ($radius * cos( deg2rad($deg))));
+			$y1 = floor($centery + ($radius * sin( deg2rad($deg))));
+
+			$innerx1 = floor($centerx + (($radius - $smallrad) * cos( deg2rad($deg))));
+			$innery1 = floor($centery + (($radius - $smallrad) * sin( deg2rad($deg))));
+
+			if ($i == 0) {
+				$symmetryshape[] = array($innerx1, $innery1);
+			}
+
+			$s .= '<circle r="'.$smallrad.'" cx="'.$x1.'" cy="'.$y1.'" stroke="black" stroke-width="'.$stroke.'"';
+			if ($scale & (1 << $i)) {
+				$s .= ' fill="black"';
+			} else {
+				$s .= ' fill="white"';
+			}
+			$s .= '/>';
+
+			if ($showImperfections) {
+				if (in_array($i, $imperfections)) {
+					$s .= '<text style="font-family: Times New Roman;font-weight:bold;font-style:italic;font-size:30px;" text-anchor="middle" x="'.$x1.'" y="'. ($y1 + 9) .'" fill="white">i</text>';
+				}
+				if (in_array($i, $symmetries)) {
+					$symmetryshape[] = array($innerx1, $innery1);
+				}
+			}
+		}
+		if (count($symmetryshape) > 1) {
+			for ($i = 0; $i < count($symmetryshape) - 1; $i++) {
+				$s .= '<line x1="'.$symmetryshape[$i][0].'" y1="'.$symmetryshape[$i][1].'" x2="'.$symmetryshape[$i+1][0].'" y2="'.$symmetryshape[$i+1][1].'" style="stroke:#000;stroke-width:'.$stroke.'" />';
+			}
+			$s .= '<line x1="'.$symmetryshape[count($symmetryshape)-1][0].'" y1="'.$symmetryshape[count($symmetryshape)-1][1].'" x2="'.$symmetryshape[0][0].'" y2="'.$symmetryshape[0][1].'" style="stroke:#000;stroke-width:'.$stroke.'" />';
+		}
+		if (!empty($text)) {
+			$s .= '<text style="font-weight: bold;" text-anchor="middle" x="'.$centerx.'" y="'. ($centery + 5) .'" fill="black">'.$text.'</text>';
+		}
+		$s .= '</svg>';
+		return $s;
+	}
+
+	function isPalindromic($scale) {
+		for ($i=1; $i<=5; $i++) {
+			if ( (bool)($scale & (1 << $i)) !== (bool)($scale & (1 << (12 - $i))) ) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
---
+++ src/PHPMusicTools/test/README.md
@@ -0,0 +1,3 @@
+Naturally, the goal is to have a phpunit test in here for every method of every class.
+
+Laziness sometimes prevails, and code gets written without tests. If you're feeling heroic, find something that isn't tested, and smother it.
